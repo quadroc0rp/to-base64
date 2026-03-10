743a7461fd76c2adf0de07e71c7028d4e3307956
Nick <quadrocorp@hotmail.com>
2026-03-11 00:59:09 +0300

    chore: add JSdoc

--- src/loader.ts
+++ src/loader.ts
@@ -1,5 +1,11 @@
 import * as vscode from "vscode";
 
+/**
+ * Reads a binary file from the specified URI.
+ *
+ * @param uri - The URI of the file to read
+ * @returns A Promise resolving to the binary data as a Uint8Array
+ */
 export async function readBinaryFile(uri: vscode.Uri): Promise<Uint8Array> {
 	return await vscode.workspace.fs.readFile(uri);
 }
